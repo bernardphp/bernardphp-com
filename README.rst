f2cb995af7bfe62abedad120638d41e1887aa872
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-02-06 08:43:49 +0100

    try  try again

--- README.rst
+++ README.rst
@@ -16,13 +16,13 @@ Download the package or add it to your ``requirements.txt`` file:
 
 .. code:: bash
 
-    $ pip install -e git+https://github.com/bernardphp/bernardphp-com.git#egg=sphinx_bernard_theme
+    $ pip install sphinx_bernard_theme
 
 In your ``conf.py`` file:
 
 .. code:: python
 
-     import sphinx_rtd_theme
+     import sphinx_bernard_theme
 
      html_theme = "sphinx_bernard_theme"
      html_theme_path = [sphinx_bernard_theme.get_html_theme_path()]
--- setup.py
+++ setup.py
@@ -3,26 +3,14 @@ from setuptools import setup
 
 setup(
     name='sphinx_bernard_theme',
-    version='1.0.0',
+    version='0.1.2',
     url='https://github.com/bernardphp/bernardphp-com/',
     license='MIT',
     author='Henrk Bjornskov',
     author_email='henrik@bjrnskov.dk',
-    description='Theme for Bernard repositories and bernardphp.com.',
-    long_description=open('README.rst').read(),
-    zip_safe=False,
     packages=['sphinx_bernard_theme'],
-    package_data={'sphinx_bernard_theme': [
-        'theme.conf',
-        '*.html',
-        'static/bernard.css_t',
-        'static/img/*.png',
-        'static/*.css',
-        'static/bootstrap/css/*.css',
-        'static/bootstrap/fonts/*.*',
-    ]},
     include_package_data=True,
-    install_requires=open('requirements.txt').read().splitlines(),
-    classifiers=[
+    install_requires=[
+        'setuptools'
     ],
 )
