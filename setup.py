15004ef6efe6f2e86a71868ee88d2cf7068ebbda
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-02-06 13:21:41 +0100

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sphinx_bernard_theme',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/bernardphp/bernardphp-com/',
     license='MIT',
     author='Henrk Bjornskov',
