c286bdd8b29cb266cf12a3d8ddd08ca1b9dc6c19
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-02-06 19:31:21 +0100

    add missing js stuff

--- sphinx_bernard_theme/static/bernard.js
+++ sphinx_bernard_theme/static/bernard.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $('.navbar-toggle').click(function () {
+    $('.navbar-toggle').click(function (e) {
         $('.toc > ul').toggle();
+
+        e.preventDefault();
     });
 });
